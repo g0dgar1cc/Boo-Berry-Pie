71c35285a9adcee5761e14e6c291f471867e5117
g0dgar1cc <ting@s.sfusd.edu>
2023-03-21 14:19:46 -0700

    added different speeds to my obstacles

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
 
-    private float speed = 20.0f;
+    public float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
